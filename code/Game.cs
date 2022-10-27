2e78dfb136bf6de0753be8598effc2bd1fd3c741
James King <james.king@facepunchstudios.com>
2022-10-27 15:14:49 +0100

    Left click to subtract

--- code/Game.cs
+++ code/Game.cs
@@ -6,10 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Sandbox.Csg;
 
-//
-// You don't need to put things in a namespace, but it doesn't hurt.
-//
-namespace Sandbox;
+namespace CsgDemo;
 
 /// <summary>
 /// This is your game class. This is an entity that is created serverside when
@@ -18,14 +15,17 @@ namespace Sandbox;
 /// You can use this to create things like HUDs and declare which player class
 /// to use for spawned players.
 /// </summary>
-public partial class MyGame : Sandbox.Game
+public partial class CsgDemoGame : Sandbox.Game
 {
-	[Net]
-	public CsgSolid CsgWorld { get; private set; }
+	public new static CsgDemoGame Current => Sandbox.Game.Current as CsgDemoGame;
+
 	public CsgBrush CubeBrush { get; } = ResourceLibrary.Get<CsgBrush>( "brushes/cube.csg" );
 	public CsgBrush DodecahedronBrush { get; } = ResourceLibrary.Get<CsgBrush>( "brushes/dodecahedron.csg" );
 
-	public MyGame()
+	[Net]
+	public CsgSolid CsgWorld { get; private set; }
+
+	public CsgDemoGame()
     {
 	}
 
@@ -42,8 +42,9 @@ public partial class MyGame : Sandbox.Game
 		}
 
 		// Create a pawn for this client to play with
-		var pawn = new Pawn();
+		var pawn = new Player();
 		client.Pawn = pawn;
+		pawn.Respawn();
 
 		// Get all of the spawnpoints
 		var spawnpoints = Entity.All.OfType<SpawnPoint>();
---
+++ code/Player.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using Sandbox;
+using Sandbox.Csg;
+
+namespace CsgDemo;
+
+partial class Player : Sandbox.Player
+{
+	public ClothingContainer Clothing { get; } = new();
+
+	public TimeSince LastJump { get; private set; }
+
+	public Player()
+	{
+
+	}
+
+	public Player( Client cl )
+	{
+		Clothing.LoadFromClient( cl );
+	}
+
+	public override void Respawn()
+	{
+		SetModel( "models/citizen/citizen.vmdl" );
+
+		Controller = new WalkController();
+		Animator = new StandardPlayerAnimator();
+		CameraMode = new ThirdPersonCamera();
+
+		Clothing.DressEntity( this );
+
+		EnableAllCollisions = true;
+		EnableDrawing = true;
+		EnableHideInFirstPerson = true;
+		EnableShadowInFirstPerson = true;
+		
+		base.Respawn();
+	}
+
+	public override void OnKilled()
+	{
+		base.OnKilled();
+
+		Controller = null;
+
+		EnableAllCollisions = false;
+		EnableDrawing = false;
+	}
+	
+	public override void Simulate( Client cl )
+	{
+		base.Simulate( cl );
+
+		if ( Input.Pressed( InputButton.Jump ) )
+		{
+			if ( LastJump < 0.25f )
+			{
+				if ( Controller is NoclipController )
+				{
+					Controller = new WalkController();
+				}
+				else
+				{
+					Controller = new NoclipController();
+				}
+			}
+
+			LastJump = 0f;
+		}
+
+		if ( !IsServer ) return;
+		
+		if ( Input.Pressed( InputButton.PrimaryAttack ) )
+		{
+			var ray = new Ray( EyePosition, EyeRotation.Forward );
+
+			if ( Trace.Ray( ray, 8192f )
+				    .Ignore( this )
+				    .Run() is { Hit: true, Entity: CsgSolid solid, HitPosition: var pos } )
+			{
+				Log.Info( "Hit!" );
+
+				solid.Modify( CsgDemoGame.Current.DodecahedronBrush, CsgOperator.Subtract, pos, Random.Shared.NextSingle() * 128f + 128f, Rotation.Random );
+			}
+		}
+	}
+}
